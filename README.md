b83720dc859bb873ff29650d1278d17c872a625e
rubic <adamtheball@gmail.com>
2019-05-21 22:21:52 -0700

    using async, now capable of accessing the google spreadsheet

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pokepartnersheet
 
 a project for punching data about pokemon into a spreadsheet about pokemon, so we don't have to write all that stuff manually.
+
+**to access the google spreadsheet** i created a service account according to the [google-spreadsheet npm directions for authentication](https://www.npmjs.com/package/google-spreadsheet#service-account-recommended-method). the google developer console spit out a .json file that i put at /ignore/account-info.json (not in this project for security reasons). 
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-let dotenv = require('dotenv');
+let dotenv = require('dotenv').config();
 let GoogleSpreadsheet = require('google-spreadsheet');
+let async = require('async');
 
+let gs = new GoogleSpreadsheet(process.env.SHEET_ID);
+let accountInfo = require('./ignore/account-info.json');
+
+async.series([
+	function SetAuth(step) {
+		gs.useServiceAccountAuth(accountInfo, (err) => {
+			if(err) console.log('useServiceAccountAuth error: ' + err);
+			step();
+		});
+	},
+	function GetInfo(step) {
+		gs.getInfo((err, info) => {
+			if(err) console.log('getInfo error: ' + err);
+			console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+			step();
+		});
+	},
+], (err) => {
+	if(err) console.log('async series error: ' + err);
+});
+
+//console.log(gs);
+//gs.getInfo(console.log);
+//console.log(gs.isAuthActive());

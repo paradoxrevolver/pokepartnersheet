dcc55512588a2eb88a1d63113e31eaf3e07580ec
rubic <adamtheball@gmail.com>
2019-05-21 23:19:33 -0700

    writing pokeapi results to file... next is to fetch the sprites

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-let dotenv = require('dotenv').config();
-let GoogleSpreadsheet = require('google-spreadsheet');
+let fs = require('fs');
 let async = require('async');
+let dotenv = require('dotenv').config();
+let Pokedex = require('pokedex-promise-v2');
+let GoogleSpreadsheet = require('google-spreadsheet');
 
 let gs = new GoogleSpreadsheet(process.env.SHEET_ID);
+let p = new Pokedex();
 let accountInfo = require('./ignore/account-info.json');
 
+let DATA_PATH = './data/';
+let FILE_NAME = 'pokedex.json';
+
 async.series([
+	function FetchPokemonList(step) {
+		p.getPokemonsList().then((response) => {
+			fs.writeFile(DATA_PATH + FILE_NAME, JSON.stringify(response, null, 4), (err) => {
+				if(err) console.log('writing pokemon list to file error: ' + err)
+			});
+		}).catch((err) => {
+			console.log('getPokemonsList error: ' + err)
+		});
+		step();
+	},
+	function FetchPokemonSprites(step) {
+		
+	},
 	function SetAuth(step) {
 		gs.useServiceAccountAuth(accountInfo, (err) => {
 			if(err) console.log('useServiceAccountAuth error: ' + err);
@@ -22,7 +41,3 @@ async.series([
 ], (err) => {
 	if(err) console.log('async series error: ' + err);
 });
-
-//console.log(gs);
-//gs.getInfo(console.log);
-//console.log(gs.isAuthActive());
